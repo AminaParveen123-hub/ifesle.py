dfbc0d2b09023e6754d2c98aa33951f37b5184ac
AminaParveen123-hub <www.amdigitalmarketing224@gmail.com>
2024-10-01 23:18:12 +0500

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,29 @@
+# Topic9 If else condition
+# Indenataion means space in python Means next block is start
+
+# one more thing All flies are running pyhton in main.file 
+# but if u want to make a new file then open the + tab select shell metion "python filename.py" then run your new file 
+a = int(input("Enter your age :"))
+print(a)
+
+#   # Convert the input to an integer
+# a = int(a)
+
+if (a == 18):
+    print("You can drive a motorbike")
+elif (a > 18):
+    print("You can drive a car")
+else:
+    print("You can not drive")
+  # Task2
+number=int(input("Enter the number :"))
+if(number%2==0):
+  print("Number is Even")
+elif(number%2!=0):
+  print("Number is Odd")
+else:
+  print("Number is special ")
+  # An if within anaother if is nested if write a code for this
+
+
+        
